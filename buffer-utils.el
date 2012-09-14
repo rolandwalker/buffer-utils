d23499cc1fa97cb4e8a707801a8c468140ce50d3
Roland Walker <walker@pobox.com>
2012-09-14 15:18:51 -0400

    bump version

--- buffer-utils.el
+++ buffer-utils.el
@@ -5,8 +5,8 @@
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/buffer-utils
 ;; URL: http://raw.github.com/rolandwalker/buffer-utils/master/buffer-utils.el
-;; Version: 0.0.2
-;; Last-Updated: 5 Sep 2012
+;; Version: 0.0.3
+;; Last-Updated: 14 Sep 2012
 ;; EmacsWiki: BufferUtils
 ;; Keywords: extensions
 ;;
