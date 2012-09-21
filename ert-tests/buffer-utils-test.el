cc41771f097b87431ec5978e3c8ce88490b885a5
Roland Walker <walker@pobox.com>
2012-09-21 13:39:38 -0400

    include build and autoload with test target add stub test file no individual tests defined, though

---
+++ ert-tests/buffer-utils-test.el
@@ -0,0 +1,20 @@
+
+;; requires and setup
+
+(require 'buffer-utils)
+
+;;; this is a stub - no tests defined
+
+;;
+;; Emacs
+;;
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; mangle-whitespace: t
+;; require-final-newline: t
+;; coding: utf-8
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+;;
+
+;;; buffer-utils-test.el ends here
